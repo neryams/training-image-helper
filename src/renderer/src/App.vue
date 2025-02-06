6b54f3cd3ecbe6965816d87744d6dcbe7e7c298b
Ray O <neryams@gmail.com>
2025-02-05 23:59:00 -0600

    update electron

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import Versions from './components/Versions.vue'
 import FileExplorer from './components/FileExplorer.vue'
-
-const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
